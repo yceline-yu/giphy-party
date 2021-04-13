27816c14a7fcb5432366befb906bb8a24f5d255e
yceline-yu <cellystuff5@gmail.com>
2021-04-13 16:50:30 -0700

    click event handler

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 console.log("Let's get this party started!");
 
 let formValue;
-$("#submit").on("click", )
+$("#submit").on("click", getGiphyInfo);
 //$("#remove").on("click", removeGif)
 
 //gets form value to use axios.get on to search for gifs
-function getGiphyInfo() {
-
+async function getGiphyInfo() {
+console.log("I ran!")
     let q = $("#api-giphy").val();
-    axios.get("http://api.giphy.com/v1/gifs/search", {params:{q:formValue,api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}})
-    appendToSection();
+    console.log(q);
+    let searchedGif = await axios.get("http://api.giphy.com/v1/gifs/search", {params:{q:q,api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}})
+    console.log(searchedGif);
+    appendToSection(searchedGif);
 }
 
 //appends gifs to right section
