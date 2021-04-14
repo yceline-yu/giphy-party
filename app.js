fb196d898441b9d757b80524ced29adc5b773ec8
S T <itschibits@gmail.com>
2021-04-13 20:38:56 -0400

    changed variable names

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 "use strict";
 console.log("Let's get this party started!");
 
-let formValue;
 $("#submit").on("click", getGiphyInfo);
 $("#remove").on("click", removeGif)
 
@@ -9,22 +8,23 @@ $("#remove").on("click", removeGif)
 async function getGiphyInfo(evt) {
     evt.preventDefault();
     console.log("I ran!")
-    let q = $("#api-giphy").val();
+    let q = $("#searchedTerm").val();
     console.log(q);
-    let searchedGif = await axios.get("http://api.giphy.com/v1/gifs/search", {params:{q:q,api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}})
-    console.log(searchedGif);
-    let firstGif = searchedGif.data.data[0].images.preview_gif.url
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", 
+        { params: { q: q, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" } })
+    console.log(response);
+    let firstGif = response.data.data[0].images.preview_gif.url
     console.log(firstGif);
     appendToSection(firstGif);
 }
 
 //appends gifs to right section
-function appendToSection(firstGif){
+function appendToSection(firstGif) {
     $("#all-gifs").append(`<img src="${firstGif}">`);
 }
 
 //removes all gifs when remove button is clicked
-function removeGif(){
+function removeGif() {
     $("#all-gifs").empty();
 }
 
