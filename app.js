c8295ccd5a4a9c50fcfd698a5c0b307ec7dab23c
S T <itschibits@gmail.com>
2021-04-13 20:13:45 -0400

    gifs append and can be removed

--- app.js
+++ app.js
@@ -3,23 +3,28 @@ console.log("Let's get this party started!");
 
 let formValue;
 $("#submit").on("click", getGiphyInfo);
-//$("#remove").on("click", removeGif)
+$("#remove").on("click", removeGif)
 
 //gets form value to use axios.get on to search for gifs
 async function getGiphyInfo(evt) {
     evt.preventDefault();
-console.log("I ran!")
+    console.log("I ran!")
     let q = $("#api-giphy").val();
     console.log(q);
     let searchedGif = await axios.get("http://api.giphy.com/v1/gifs/search", {params:{q:q,api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}})
     console.log(searchedGif);
-    appendToSection(searchedGif);
+    let firstGif = searchedGif.data.data[0].images.preview_gif.url
+    console.log(firstGif);
+    appendToSection(firstGif);
 }
 
 //appends gifs to right section
-function appendToSection(){
-
+function appendToSection(firstGif){
+    $("#all-gifs").append(`<img src="${firstGif}">`);
 }
 
-//function removeGif(){}
+//removes all gifs when remove button is clicked
+function removeGif(){
+    $("#all-gifs").empty();
+}
 
